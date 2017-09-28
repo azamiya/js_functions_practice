ae629c4d1cb7e6b0f12fd0c2baa2a8a287df24b1
cdra0624 <t.adaniya@adawarp.com>
2017-09-28 10:43:30 +0900

    Add the function rember

--- main.js
+++ main.js
@@ -28,10 +28,22 @@ function isLat(arg) {
 
 function isMember(arg1, arg2) {
   if (arg2 == []) return false;
-  for(i =0; i < arg2.length; i++) {
+  for(i = 0; i < arg2.length; i++) {
     if(arg1 == arg2[i]) {
       return true;
     }
   }
   return false;
 }
+
+function rember(atom, lat) {
+  let l = [];
+  if (lat == []) return l;
+  for (i = 0; i < lat.length; i++) {
+    if(atom != lat[i]) {
+      l.push(lat[i]);
+    }
+  }
+  return l;
+}
+
